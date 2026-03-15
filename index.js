6199ff5b52e4a8b814a051ae9f4f381eb1af7cd8
Sedat Bakla <sedatbkl7@gmail.com>
2026-03-15 17:53:23 +0300

    Sedat Bakla gereksinimleri: Room model, controller ve routes eklendi

---
+++ index.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import connectDB from './src/config/db.js';
+
+import aiRoutes from './src/routes/aiRoutes.js';
+import notifyRoutes from './src/routes/notifyRoutes.js';
+import roomRoutes from './src/routes/roomRoutes.js';
+
+const app = express();
+connectDB();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.json({ message: `VQuest Backend is running on port ${port}!` });
+});
+
+// API Routes
+app.use('/api', aiRoutes);
+app.use('/api', notifyRoutes);
+app.use('/api', roomRoutes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
